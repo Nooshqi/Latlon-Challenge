0aab5e2bfcfe64db0539ccd7a37e0de27d0cbff2
BRYAN KINUTHIA <bkinuthi@uwaterloo.ca>
2020-05-08 03:56:21 -0400

    Comments and Formatting

--- ConsoleApp1/JsonFeed.cs
+++ ConsoleApp1/JsonFeed.cs
@@ -10,103 +10,109 @@ using Newtonsoft.Json;
 namespace ConsoleApp1
 {
     class JsonFeed
-    {	
-		private static HttpClient _jokeClient = new HttpClient();
-		private static HttpClient _nameClient = new HttpClient();
-		private static HttpClient _categoryClient = new HttpClient();
+    {
+		//declare respective clients
+        private static HttpClient _jokeClient = new HttpClient();
+        private static HttpClient _nameClient = new HttpClient();
+        private static HttpClient _categoryClient = new HttpClient();
         static string _url = "";
-		static int count;
-		static Tuple<string, string> names;
+        static Tuple<string, string> names;
 
-		
 
-        public JsonFeed(string endpoint) {
-			_url = endpoint;
-		}
-        public JsonFeed(string endpoint, int results)
-        {	
+        public JsonFeed() { }
+
+        public JsonFeed(string endpoint)
+        {
             _url = endpoint;
-			count = results;
         }
-        
-		public string[] GetRandomJokes(string category, int count)
-		{	
-			if (_jokeClient.BaseAddress == null){
-				_jokeClient.BaseAddress = new Uri(_url);
-			}
-			string[] jokes = new String[count];
 
-			for(int i=0; i<count; i++){
-				string sol = null;
-				string url = "random";
-				if (category != null){
-					url = url +"?" + "category=" + category;
-				}
+        public string[] GetRandomJokes(string category, int count)
+        {
+            if (_jokeClient.BaseAddress == null)
+            {
+                _jokeClient.BaseAddress = new Uri(_url);
+            }
+            string[] jokes = new String[count];
 
-				var responseTask = _jokeClient.GetAsync(url);
-				responseTask.Wait();
-				var result = responseTask.Result;
-				if (result.IsSuccessStatusCode){
-					var readTask = result.Content.ReadAsStringAsync();
-					readTask.Wait();
-					sol = JsonConvert.DeserializeObject<dynamic>(readTask.Result).value;
-				}
-				else sol = result.StatusCode.ToString() + "~ There was an issue reaching the API, might be an illegal Category";
+			//loop through generating the requested number of jokes
+            for (int i = 0; i < count; i++)
+            {
+                string sol = null;
+                string url = "random";
+                if (category != null)
+                {
+                    url = url + "?" + "category=" + category;
+                }
 
-				//Replace Chuck Norris with a random name
-				if (names!=null)
-				{
-					sol = NameChange(sol, names.Item1, names.Item2);
-				}
-				
-				jokes[i] = sol;
+                var responseTask = _jokeClient.GetAsync(url);
+                responseTask.Wait();
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+                    sol = JsonConvert.DeserializeObject<dynamic>(readTask.Result).value;
+                }
+                else sol = result.StatusCode.ToString() + "~ There was an issue reaching the API, might be an illegal Category";
 
-			}
-			names = null;
+                //Replace Chuck Norris with a random name
+                if (names != null)
+                {
+                    sol = NameChange(sol, names.Item1, names.Item2);
+                }
+
+                jokes[i] = sol;
+
+            }
+            names = null;
             return jokes;
         }
-		public static string NameChange(string joke,string firstN,string lastN){
-			return joke.Replace("Chuck",firstN).Replace("Norris",lastN);
-		}
+        public static string NameChange(string joke, string firstN, string lastN)
+        {
+            return joke.Replace("Chuck", firstN).Replace("Norris", lastN);
+        }
 
-		public void Getnames()
-		{
-			dynamic sol;
-			if (_nameClient.BaseAddress == null)
-			{
-				_nameClient.BaseAddress = new Uri("https://names.privserv.com/api/");
-			}
-			var responseTask = _nameClient.GetAsync("");
-			responseTask.Wait();
-			var result = responseTask.Result;
-			if (result.IsSuccessStatusCode){
-				var readTask = result.Content.ReadAsStringAsync();
-				readTask.Wait();
-				sol = JsonConvert.DeserializeObject<dynamic>(readTask.Result);
-				names = Tuple.Create(sol.name.ToString(), sol.surname.ToString());
-			}
-			else sol = result.StatusCode.ToString();
-		}
+        //Helper method used to fetch random names through a consumed API call
+        public void Getnames()
+        {
+            dynamic sol;
+            if (_nameClient.BaseAddress == null)
+            {
+                _nameClient.BaseAddress = new Uri("https://names.privserv.com/api/");
+            }
+            var responseTask = _nameClient.GetAsync("");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsStringAsync();
+                readTask.Wait();
+                sol = JsonConvert.DeserializeObject<dynamic>(readTask.Result);
+                names = Tuple.Create(sol.name.ToString(), sol.surname.ToString());
+            }
+            else sol = result.StatusCode.ToString();
+        }
 
 
-		public String GetCategories(){
-			string sol = null;
+        public String GetCategories()
+        {
+            string sol = null;
+            if (_categoryClient.BaseAddress == null)
+            {
+                _categoryClient.BaseAddress = new Uri(_url);
+            }
 
-			if (_categoryClient.BaseAddress == null)
-			{
-				_categoryClient.BaseAddress = new Uri(_url);
-			}
-
-			var responseTask = _categoryClient.GetAsync("categories");
-			responseTask.Wait();
-			var result = responseTask.Result;
-			if (result.IsSuccessStatusCode){
-				var readTask = result.Content.ReadAsStringAsync();
-				readTask.Wait();
-				sol = readTask.Result;
-			}
-			else sol = result.StatusCode.ToString();
-			return sol;
-		}
+            var responseTask = _categoryClient.GetAsync("categories");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsStringAsync();
+                readTask.Wait();
+                sol = readTask.Result;
+            }
+            else sol = result.StatusCode.ToString();
+            return sol;
+        }
     }
 }
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -13,32 +13,35 @@ namespace ConsoleApp1
         static string[] results = new string[50]; 
         static string output;
         static char key;
-        //static Tuple<string, string> names;
         static ConsolePrinter printer = new ConsolePrinter();
 
         static void Main(string[] args)
         {   
-            //Declare instance of JsonFeed
+            //Declare single instance of JsonFeed
             JsonFeed jsonfd = new JsonFeed("https://api.chucknorris.io/jokes/");
 
 
-            printer.Print("Welcome. Press ? to get started. Press any key to leave");
+            printer.Print("Welcome. Press '?' followed by ENTER to get started. Press any key to leave");
             if (Console.ReadLine() == "?")
             {
                 while (true)
                 {
-                    printer.Print("Press C to get JOKE CATEGORIES or Press R to get RANDOM JOKES. Q to quit");
+                    printer.Print("Press C to get JOKE CATEGORIES or Press R to get RANDOM JOKES. Q to QUIT");
                     GetEnteredKey(Console.ReadKey());
-                    if (key == 'c'){
+                    if (key == 'c')
+                    {
                         output = jsonfd.GetCategories();
                         printer.Print(output);
                     }
                     else if (key == 'r')
                     {
-                        printer.Print('\n'+"Press Y if you want a random name else we'll stick to Chuck Norris");
+                        printer.Print('\n'+"Press Y if you want a RANDOM NAME else we'll stick to CHUCK NORRIS");
                         GetEnteredKey(Console.ReadKey());
-                        if (key == 'y'){jsonfd.Getnames();}
-                        printer.Print('\n'+"How many jokes do you want? (1-9) then press Enter");
+                        if (key == 'y')
+                        {
+                            jsonfd.Getnames();
+                        }
+                        printer.Print('\n'+"How many jokes do you want? (1-9) then press ENTER. Wrong entries default to 1");
                         int n;
 
                         if (int.TryParse(Console.ReadLine(), out n)){
@@ -49,19 +52,24 @@ namespace ConsoleApp1
                             n=1;
                         }
 
-                        printer.Print("Press Y if you want to specify a category else we'll have it unspecified");
+                        printer.Print("Press Y if you want to specify a CATEGORY else we'll leave it unspecified");
                         GetEnteredKey(Console.ReadKey());
-                        if (key == 'y'){   
+                        if (key == 'y')
+                        {   
                             printer.Print('\n'+"Enter a category;");
                             results = jsonfd.GetRandomJokes(Console.ReadLine(), n);
                             printer.Print(results);
                         }
-                        else{
+                        else
+                        {
                             results = jsonfd.GetRandomJokes(null, n);
                             printer.Print(results);
                         }
                     }
-                    else if (key == 'q'){break;}
+                    else if (key == 'q')
+                    {
+                        break;
+                    }
                     else printer.Print('\n'+"Sorry, Wrong Input");
                 }
             }
