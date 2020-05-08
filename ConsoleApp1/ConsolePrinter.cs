e63329d668a5c027ca945f5cdf241f6a2c341c25
BRYAN KINUTHIA <bkinuthi@uwaterloo.ca>
2020-05-08 03:14:39 -0400

    Initial commit

---
+++ ConsoleApp1/ConsolePrinter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    public class ConsolePrinter
+    {
+        public void Print(string value){
+            Console.WriteLine('\n'+value);
+        }
+        public void Print(string[] values){
+            foreach(string value in values){
+                Console.WriteLine('\n'+value);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static string[] results = new string[50]; 
+        static string output;
+        static char key;
+        //static Tuple<string, string> names;
+        static ConsolePrinter printer = new ConsolePrinter();
+
+        static void Main(string[] args)
+        {   
+            //Declare instance of JsonFeed
+            JsonFeed jsonfd = new JsonFeed("https://api.chucknorris.io/jokes/");
+
+
+            printer.Print("Welcome. Press ? to get started. Press any key to leave");
+            if (Console.ReadLine() == "?")
+            {
+                while (true)
+                {
+                    printer.Print("Press C to get JOKE CATEGORIES or Press R to get RANDOM JOKES. Q to quit");
+                    GetEnteredKey(Console.ReadKey());
+                    if (key == 'c'){
+                        output = jsonfd.GetCategories();
+                        printer.Print(output);
+                    }
+                    else if (key == 'r')
+                    {
+                        printer.Print('\n'+"Press Y if you want a random name else we'll stick to Chuck Norris");
+                        GetEnteredKey(Console.ReadKey());
+                        if (key == 'y'){jsonfd.Getnames();}
+                        printer.Print('\n'+"How many jokes do you want? (1-9) then press Enter");
+                        int n;
+
+                        if (int.TryParse(Console.ReadLine(), out n)){
+                            n = (n<1||n>9)?1:n*1;
+                        }
+                        else{
+                            printer.Print("Wrong input. We'll stick to one joke");
+                            n=1;
+                        }
+
+                        printer.Print("Press Y if you want to specify a category else we'll have it unspecified");
+                        GetEnteredKey(Console.ReadKey());
+                        if (key == 'y'){   
+                            printer.Print('\n'+"Enter a category;");
+                            results = jsonfd.GetRandomJokes(Console.ReadLine(), n);
+                            printer.Print(results);
+                        }
+                        else{
+                            results = jsonfd.GetRandomJokes(null, n);
+                            printer.Print(results);
+                        }
+                    }
+                    else if (key == 'q'){break;}
+                    else printer.Print('\n'+"Sorry, Wrong Input");
+                }
+            }
+            else printer.Print("Goodbye");
+
+        }
+
+        private static void GetEnteredKey(ConsoleKeyInfo consoleKeyInfo)
+        {
+            switch (consoleKeyInfo.Key)
+            {
+                case ConsoleKey.C:
+                    key = 'c';
+                    break;
+                case ConsoleKey.R:
+                    key = 'r';
+                    break;
+                case ConsoleKey.Y:
+                    key = 'y';
+                    break;
+                default:
+                    key = 'q';
+                    break;
+            }
+        }
+    }
+}
